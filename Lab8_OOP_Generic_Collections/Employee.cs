806836db6b8da9e55d866bc97eb5f6b65b17053b
lucine1029 <sherry.jing@hotmail.com>
2023-10-16 11:03:01 +0200

    Add project files.

---
+++ Lab8_OOP_Generic_Collections/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_OOP_Generic_Collections
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double MonthlySalary { get; set; }
+        public Employee(int id, string name, string gender, double monthlySalary)
+        {
+            Id = id;
+            Name = name;
+            Gender = gender;
+            MonthlySalary = monthlySalary;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{Id} - {Name} - {Gender} - {MonthlySalary}");
+        }
+        public override string ToString()
+        {
+            return $"ID = {Id}, Name = {Name}, Gender = {Gender}, MonthlySalary = {MonthlySalary}";
+        }
+
+    }
+}
---
+++ Lab8_OOP_Generic_Collections/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace Lab8_OOP_Generic_Collections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Del 1 -Stack");
+            //Del 1 - Stack
+            Employee employee1 = new Employee(101, "Alex", "Male", 30000);
+            Employee employee2 = new Employee(102, "Sara", "Female", 40000);
+            Employee employee3 = new Employee(103, "Hanna", "Female", 30000);
+            Employee employee4 = new Employee(104, "Tomas", "Male", 50000);
+            Employee employee5 = new Employee(105, "Kalle", "Male", 20000);
+
+            Stack<Employee> EmployeeStack = new Stack<Employee>();
+            EmployeeStack.Push(employee1);
+            EmployeeStack.Push(employee2);
+            EmployeeStack.Push(employee3);
+            EmployeeStack.Push(employee4);
+            EmployeeStack.Push(employee5);
+
+            foreach (Employee emp in EmployeeStack)
+            {
+                emp.Display();
+                Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count}");
+            }
+            //Retrive using pop method
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Retrive Using Pop Method");
+            Employee emp5 = EmployeeStack.Pop();
+            emp5.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+            Employee emp4 = EmployeeStack.Pop();
+            emp4.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+            Employee emp3 = EmployeeStack.Pop();
+            emp3.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+            Employee emp2 = EmployeeStack.Pop();
+            emp2.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+            Employee emp1 = EmployeeStack.Pop();
+            emp1.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+
+            Console.WriteLine("-------------------------------");
+            //Add all objects again
+            EmployeeStack.Push(emp1);
+            EmployeeStack.Push(emp2);
+            EmployeeStack.Push(emp3);
+            EmployeeStack.Push(emp4);
+            EmployeeStack.Push(emp5);
+            //Retrive using peek method
+            Console.WriteLine("Retrive Using Peek Method");
+            Employee empOne = EmployeeStack.Peek();
+            empOne.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+            Employee empTwo = EmployeeStack.Peek();
+            empTwo.Display();
+            Console.WriteLine($"Item left in the Stack = {EmployeeStack.Count()}");
+            Console.WriteLine("-------------------------------");
+            if (EmployeeStack.Contains(emp3))
+            {
+                Console.WriteLine("Emp3 is in stack");
+            }
+            else
+            {
+                Console.WriteLine("Emp3 is not in stack");
+            }
+
+            // Del 2 - List
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Del 2 - List");
+            var employeeList = new List<Employee>() {};
+            employeeList.Add(employee1);
+            employeeList.Add(employee2);
+            employeeList.Add(employee3);
+            employeeList.Add(employee4);
+            employeeList.Add(employee5);
+            //use Contains() method to check if the specified element exists or not in a list<T>
+            if (employeeList.Contains(employee2))
+            {
+                Console.WriteLine("EmployeeTwo object exists in the list");
+            }
+            else
+            {
+                Console.WriteLine("EmployeeTwo object does not exit in the list");
+            }
+            Console.WriteLine();
+
+            // Use Find() method to find the first male 
+
+            Employee firstMale = employeeList.Find(x => x.Gender == "Male");
+            if (firstMale != null)
+            {
+                Console.WriteLine(firstMale.ToString());
+            }
+            else
+            {
+                Console.WriteLine("We can't find male employees");
+            }
+            Console.WriteLine();
+            //  Use FindAll() method to find all males
+            List<Employee> allMales = employeeList.FindAll(x => x.Gender == "Male");
+            if (allMales.Count > 0)
+            {
+                foreach (Employee e in allMales)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("We can't find male employees");
+            }
+
+        }
+    }
+}
